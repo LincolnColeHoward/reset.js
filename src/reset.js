1d902836c343567d623fdfc12a27f242452a12f4
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2016-12-04 11:10:51 -0500

    new version

--- src/reset.js
+++ src/reset.js
@@ -1,129 +1,227 @@
-// create a dom element
-function DOM (element) {
-	var list = element.split (".");
-	var ret = document.createElement (list [0]);
-	for (var i = 1; i < list.length; i++)
-		ret.classList.add (list [i]);
-	return ret;
-}
-// create a dom element that is a child of this
-Element.prototype.DOM = function (element) {
-	var ret = DOM (element);
-	this.appendChild (ret);
-	return ret;
-}
-// routes
-function GET (route, _opts) {
-	var opts = {
-		cb: false,
-		err: false,
-		id: false
-	}
-	Object.assign (opts, _opts);
-	var path = route;
-	if (opts.id) path += "/" + opts.id;
-	var xhr = new XMLHttpRequest ();
+'use strict'
+// get json via xhr
+let GET = function (path, cb, err) {
+	let xhr = new XMLHttpRequest ();
 	xhr.open ("GET", path);
 	xhr.onload = function () {
-		if (xhr.status === 200) {
-			if (opts.cb) {
-				try {
-					return opts.cb (JSON.parse (this.response));
-				} catch (e) {
-					return opts.cb (this.response);
-				}
-			}
-		} else {
-			if (opts.err) return opts.err (xhr.status);
+		if (xhr.status !== 200 && err) return err ();
+		try {
+		  cb (JSON.parse (xhr.response));
+		} catch (e) {
+		  cb (xhr.response);
 		}
 	}
 	xhr.send ();
 }
-// post
-function POST (route, _opts) {
-	var opts = {
-		data: null,
-		cb: false,
-		err: false
-	}
-	Object.assign (opts, _opts);
-	var xhr = new XMLHttpRequest ();
-	xhr.open ("POST", route);
-	xhr.setRequestHeader ("Content-Type", "application/json");
+// post json via xhr
+let POST = function (path, data, cb, err) {
+	let xhr = new XMLHttpRequest ();
+	xhr.open ("POST", path);
+	xhr.setRequestHeader ('Content-Type', 'application/json');
 	xhr.onload = function () {
-		if (xhr.status === 201) {
-			if (opts.cb && this.response) {
-				try {
-					return opts.cb (JSON.parse (this.response));
-				} catch (e) {
-					return opts.cb (this.response);
-				}
-			}
-			if (opts.cb && !this.response) return opts.cb ();
-		} else {
-			if (opts.err) opts.err (xhr.status);
+		if (xhr.status !== 201 && err) return err ();
+		cb (JSON.parse (xhr.response));
+	}
+	xhr.send (JSON.stringify (data));
+}
+// create element and automatically append
+let dom = function (el, str) {
+	let list = str.split (".");
+	let ret = document.createElement (list [0]);
+	for (let i = 1; i < list.length; i++)
+		ret.classList.add (list [i]);
+	el.appendChild (ret);
+	return ret;
+}
+// set innerHTML value
+let html = function (el, value) {
+	el.innerHTML = value;
+	return el;
+}
+// set k/v for a property
+let prop = function (el, property, value) {
+	el.setAttribute (property, value);
+	return el;
+}
+// add a class to the class list
+let addClass = function (el, str) {
+	el.classList.add (str);
+	return el;
+}
+// remove all classes and add new ones
+let classes = function (el, str) {
+	let adjust = str || '';
+	let list = adjust.split ('.');
+	if (list [0] === '') list.splice (0, 1);
+	el.className = '';
+	list.forEach (function (cls) {
+		el.classList.add (cls);
+	});
+	return el;
+}
+// remove a property
+let unset = function (el, property) {
+	el.removeAttribute (property);
+	return el;
+}
+// add an event handler
+let evt = function (el, name, fn) {
+	el.addEventListener (name, fn, false);
+	return el;
+}
+// trigger an event
+let trigger = function (el, event) {
+	el.dispatchEvent (event);
+	return el;
+}
+// bind all the above methods and make chainable
+Element.prototype.dom = function (str) {
+	return dom (this, str);
+}
+Element.prototype.html = function (value) {
+	return html (this, value);
+}
+Element.prototype.addClass = function (str) {
+	return addClass (this, str);
+}
+Element.prototype.classes = function (str) {
+	return classes (this, str);
+}
+Element.prototype.prop = function (property, value) {
+	return prop (this, property, value);
+}
+Element.prototype.unset = function (property) {
+	return unset (this, property);
+}
+Element.prototype.evt = function (name, fn) {
+	return evt (this, name, fn);
+}
+Element.prototype.trigger = function (event) {
+	return trigger (this, event);
+}
+// dom for shadow root
+ShadowRoot.prototype.dom = function (str) {
+	return dom (this, str);
+}
+// create the FN alias for the NodeWrapper
+let FN = null;
+(function () {
+	// symbol to encapsulate the node list
+	let list = Symbol ();
+	FN = class NodeWrapper {
+		constructor (initial) {
+			// start with an empty array
+			this [list] = [];
+			if (initial instanceof NodeList)
+				// if provided a NodeList, turn it into an array
+				this [list] = Array.from (initial);
+			if (initial instanceof String || typeof initial === 'string')
+				// if provided a query, turn it into a nodelist, then an array
+				this [list] = Array.from (document.querySelectorAll (initial));
+		}
+		// add an element or set of elements to the array
+		// accepts queryselectorall strings 
+		add (el) {
+			if (el instanceof Element)
+				this [list].push (el);
+			if (el instanceof NodeList)
+				this [list] = this [list].concat (el)
+			if (typeof el === 'string' || el instanceof String)
+				this [list] = this [list].concat (Array.from (document.querySelectorAll (el)));
+			return this;
+		}
+		// get the element at an index
+		at (index) {
+			return this [list] [index];
+		}
+		// remove element
+		remove (el) {
+			if (typeof el === Element)
+				this [list].splice (this [list].indexOf (el), 1);
+			if (typeof el === 'string' || el instanceof String) {
+				let todelete = document.querySelector (el);
+				let index = this [list].indexOf (todelete);
+				if (todelete && index !== -1)
+					this [list].splice (index, 1);
+			} 
+			return this;
+		}
+		// get the length of the list
+		get length () {
+			return this [list].length;
+		}
+		// bind chainable method
+		// returns the list of child elements created
+		dom (str) {
+			let ret = new FN ();
+			this.forEach (function (el) {
+				ret.add (el.dom (str));
+			});
+			return ret;
+ 		}
+ 		// bind chainable method
+		html (str) {
+			this.forEach (function (el) {
+				el.html (str);
+			});
+			return this;
+		}
+		// bind chainable method
+		prop (property, value) {
+			this.forEach (function (el) {
+				el.prop (property, value);
+			});
+			return this;
+		}
+		// bind chainable method
+		unset (property) {
+			this.forEach (function (el) {
+				el.unset (property);
+			});
+			return this;
+		}
+		// bind chainable method
+		addClass (str) {
+			this.forEach (function (el) {
+				el.addClass (str);
+			});
+		}
+		// bind chainable method
+		classes (str) {
+			this.forEach (function (el) {
+				el.classes (str);
+			});
+			return this;
+		}
+		// check if element is contained
+		contains (el) {
+			return this [list].indexOf (el) !== -1;
+		}
+		// get a new NodeWrapper as the intersection of two NodeWrappers
+		// if second argument is true, only add elements that are different
+		intersect (rhs, keepDiff) {
+			let self = this;
+			let setting = keepDiff || false;
+			let ret = new FN ();
+			this.forEach (function (el) {
+				if (rhs.contains (el) && !setting)
+					ret.add (el);
+				else if (!rhs.contains (el) && setting)
+					ret.add (el);
+			});
+			return ret;
+		}
+		// bind chainable method
+		trigger (event) {
+			this.forEach (function (el) {
+				el.trigger (event);
+			});
+			return this;
+		}
+		// alias forEach method
+		forEach (fn) {
+			this [list].forEach (fn);
+			return this;
 		}
 	}
-	if (opts.data)
-		xhr.send (JSON.stringify (opts.data));
-	else xhr.send ();
-}
-// expanding textarea elements
-function Expander (textarea) {
-	textarea.addEventListener ("keyup", function () {
-		this.style.height = this.scrollHeight + "px";
-	}, false);
-}
-// create the modal
-var modal;
-// clear the modal message
-function closeMessage () {
-	modal.className = "modal";
-	modal.innerHTML = "";
-	var close = modal.DOM ("span.modal-close");
-	close.innerHTML = "close";
-	close.addEventListener ("click", closeMessage, false);
-	window.removeEventListener ("resize", modal.adjust, false);
-}
-// flash text or DOM element for a modal
-function flashMessage (text, adjust) {
-	var area;
-	if (text instanceof Element) {
-		modal.appendChild (text);
-		area = text;
-	} else {
-		area = modal.DOM ("textarea");
-		area.setAttribute ("readonly", "true");
-		area.value = text;
-		area.style.height = 0;
-		area.style.height = this.scrollHeight + "px";
-	}
-	modal.className = "modal intro";
-	modal.className = "modal intro show";
-	modal.adjust = adjust || function () {
-		var calc = {
-			w: area.scrollWidth,
-			h: area.scrollHeight
-		}
-		calc.x = (modal.clientWidth - calc.w) / 2;
-		calc.y = (modal.clientHeight - calc.h) / 2;
-		area.style.width = calc.w + "px";
-		area.style.height = calc.h + "px";
-		area.style.left = calc.x + "px";
-		area.style.top = calc.y + "px";
-	}
-	modal.adjust ();
-	window.addEventListener ("resize", modal.adjust, false);
-	return area;
-}
-
-document.addEventListener ("DOMContentLoaded", function () {
-	modal = document.body.DOM ("div");
-	// adjust the size of the modal
-	modal.adjust = function () {};
-	modal.addEventListener ("click", closeMessage, false);
-	closeMessage ();
-	var txts = document.querySelectorAll ("textarea");
-	for (var i = 0; i < txts.length; i++)
-		Expander (txts [i]);
-}, false);
+}) ();
